83b42a2687285dd6b0a0ac251dbf99dbc442a448
maniSHarma7575 <39980734+maniSHarma7575@users.noreply.github.com>
2019-11-18 11:26:18 +0530

    Create DynamicProgramming.py
    Bottom Up Dynamic Programming solution

---
+++ Dynamic Programming/Standard Problems/Longest Common Subsequence/DynamicProgramming.py
@@ -0,0 +1,20 @@
+#Longest Common subsequence Dynamic Programming solution
+#File Created by Manish Sharma
+def lcs(X , Y): 
+    m = len(X) 
+    n = len(Y) 
+    L = [[None]*(n+1) for i in range(m+1)] 
+    for i in range(m+1): 
+        for j in range(n+1): 
+            if i == 0 or j == 0 : 
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]: 
+                L[i][j] = L[i-1][j-1]+1
+            else: 
+                L[i][j] = max(L[i-1][j] , L[i][j-1]) 
+  
+    # L[m][n] contains the length of LCS of X[0..n-1] & Y[0..m-1] 
+    return L[m][n] 
+x=input()
+y=input()
+print(lcs(x,y))
